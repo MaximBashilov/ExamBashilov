42fa4380c0ed3002776de77020ea1b85e3644492
MaximBashilov <121537166+MaximBashilov@users.noreply.github.com>
2022-12-28 12:18:13 +0300

    added files

---
+++ src/ExamBashilov/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace ExamBashilov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BrowserPanel = new System.Windows.Forms.Panel();
+            this.WebBrowser = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.XLabel = new System.Windows.Forms.Label();
+            this.YLabel = new System.Windows.Forms.Label();
+            this.XTextBox = new System.Windows.Forms.TextBox();
+            this.YTextBox = new System.Windows.Forms.TextBox();
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.BrowserPanel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BrowserPanel
+            // 
+            this.BrowserPanel.Controls.Add(this.WebBrowser);
+            this.BrowserPanel.Location = new System.Drawing.Point(12, 27);
+            this.BrowserPanel.Name = "BrowserPanel";
+            this.BrowserPanel.Size = new System.Drawing.Size(1076, 467);
+            this.BrowserPanel.TabIndex = 0;
+            // 
+            // WebBrowser
+            // 
+            this.WebBrowser.AllowWebBrowserDrop = false;
+            this.WebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WebBrowser.Location = new System.Drawing.Point(0, 0);
+            this.WebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WebBrowser.Name = "WebBrowser";
+            this.WebBrowser.Size = new System.Drawing.Size(1076, 467);
+            this.WebBrowser.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1100, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // XLabel
+            // 
+            this.XLabel.AutoSize = true;
+            this.XLabel.Location = new System.Drawing.Point(12, 519);
+            this.XLabel.Name = "XLabel";
+            this.XLabel.Size = new System.Drawing.Size(235, 13);
+            this.XLabel.TabIndex = 2;
+            this.XLabel.Text = "Введите значение X (действительное число):";
+            this.XLabel.Visible = false;
+            // 
+            // YLabel
+            // 
+            this.YLabel.AutoSize = true;
+            this.YLabel.Location = new System.Drawing.Point(12, 544);
+            this.YLabel.Name = "YLabel";
+            this.YLabel.Size = new System.Drawing.Size(235, 13);
+            this.YLabel.TabIndex = 3;
+            this.YLabel.Text = "Введите значение Y (действительное число):";
+            this.YLabel.Visible = false;
+            // 
+            // XTextBox
+            // 
+            this.XTextBox.Location = new System.Drawing.Point(253, 516);
+            this.XTextBox.Name = "XTextBox";
+            this.XTextBox.Size = new System.Drawing.Size(100, 20);
+            this.XTextBox.TabIndex = 4;
+            this.XTextBox.Visible = false;
+            // 
+            // YTextBox
+            // 
+            this.YTextBox.Location = new System.Drawing.Point(253, 541);
+            this.YTextBox.Name = "YTextBox";
+            this.YTextBox.Size = new System.Drawing.Size(100, 20);
+            this.YTextBox.TabIndex = 5;
+            this.YTextBox.Visible = false;
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Location = new System.Drawing.Point(386, 516);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(101, 45);
+            this.CheckButton.TabIndex = 6;
+            this.CheckButton.Text = "Проверить";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Location = new System.Drawing.Point(0, 484);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(1100, 22);
+            this.statusStrip.TabIndex = 7;
+            this.statusStrip.Text = "123123123";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 506);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.CheckButton);
+            this.Controls.Add(this.YTextBox);
+            this.Controls.Add(this.XTextBox);
+            this.Controls.Add(this.YLabel);
+            this.Controls.Add(this.XLabel);
+            this.Controls.Add(this.BrowserPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Экзамен ПКсп-120 Башилов М.А.";
+            this.BrowserPanel.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel BrowserPanel;
+        private System.Windows.Forms.WebBrowser WebBrowser;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.Label XLabel;
+        private System.Windows.Forms.Label YLabel;
+        private System.Windows.Forms.TextBox XTextBox;
+        private System.Windows.Forms.TextBox YTextBox;
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.StatusStrip statusStrip;
+    }
+}
+
---
+++ src/ExamBashilov/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamBashilov
+{
+    public partial class Form1 : Form
+    {
+        public enum Mode
+        {
+            Ex1,
+            Ex2,
+            NotStated
+        }
+
+        private Mode mode = Mode.NotStated;
+        double x0, y0;
+        private ToolStripLabel statusLabel;
+        public Form1()
+        {
+            InitializeComponent();
+            statusLabel = new ToolStripLabel();
+            statusLabel.Text = "Строка состояния";
+
+            statusStrip.Items.Add(statusLabel);
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "html files (*.html)|*.html";
+                ofd.FilterIndex = 0;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    CheckFunction(ofd.FileName, ofd.SafeFileName);
+                }
+            }
+        }
+
+        private void CheckFunction(string path, string fileName)
+        {    
+            if (fileName == "site1.html")
+            {
+                mode = Mode.Ex1;
+                this.Size = new Size(1125, 650);
+                WebBrowser.Navigate(path);
+                ShowTextBoxes();
+            }
+            else if (fileName == "site2.html")
+            {
+                mode = Mode.Ex2;
+                this.Size = new Size(1125, 650);
+                WebBrowser.Navigate(path);
+                ShowTextBoxes();
+            }
+            else
+            {
+                mode = Mode.NotStated;
+                this.Size = new Size(1125, 545);
+                HideTextBoxes();
+                MessageBox.Show("Для работы программы нужно открыть файл с заданием!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CheckPointFirst(double x, double y)
+        {
+            double d = (x * x) + (y * y);
+            if (d < 1.0)
+            {
+                statusLabel.Text = "Точка находится внутри окружности!";
+                MessageBox.Show("Точка находится внутри окружности!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (d > 1.0)
+            {
+                statusLabel.Text = "Точка находится вне окружности!";
+                MessageBox.Show("Точка находится вне окружности!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                statusLabel.Text = "Точка находится на границе окружности!";
+                MessageBox.Show("Точка находится на границе окружности!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+
+        private void CheckPointSecond()
+        {   
+            double a, b, c;
+            double x1 = 1.0, x2 = 5.0, x3 = -5.0;
+            double y1 = 4.0, y2 = -4.0, y3 = -3.0;
+
+            a = (x1 - x0) * (y2 - y1) - (x2 - x1) * (y1 - y0);
+            b = (x2 - x0) * (y3 - y2) - (x3 - x2) * (y2 - y0);  
+            c = (x3 - x0) * (y1 - y3) - (x1 - x3) * (y3 - y0);
+
+            if (a == 0.0 || b == 0.0 || c == 0.0)
+            {
+                statusLabel.Text = "Точка находится на границе треугольника!";
+                MessageBox.Show("Точка находится на границе треугольника!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if ((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0))
+            {
+                statusLabel.Text = "Точка находится внутри треугольника!";
+                MessageBox.Show("Точка находится внутри треугольника!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                statusLabel.Text = "Точка находится вне треугольника!";
+                MessageBox.Show("Точка находится вне треугольника!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+
+        private void HideTextBoxes()
+        {
+            XLabel.Visible = false;
+            YLabel.Visible = false;
+            XTextBox.Visible = false;
+            YTextBox.Visible = false;
+            CheckButton.Visible = false;
+        }
+
+        private void ShowTextBoxes()
+        {
+            XLabel.Visible = true;
+            YLabel.Visible = true;
+            XTextBox.Visible = true;
+            YTextBox.Visible = true;
+            CheckButton.Visible = true;
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программу разработал:\nСтудент группы ПКсп-120\nБашилов Максим Андреевич\nВариант: 11", "О программе",MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            switch (mode)
+            {
+                case Mode.Ex1:
+                    try
+                    {
+                        x0 = double.Parse(XTextBox.Text);
+                        y0 = double.Parse(YTextBox.Text);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Введены некорректные данные!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    CheckPointFirst(x0, y0);
+                    break;
+                case Mode.Ex2:
+                    try
+                    {
+                        x0 = double.Parse(XTextBox.Text);
+                        y0 = double.Parse(YTextBox.Text);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Введены некорректные данные!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    CheckPointSecond();
+                    break;
+            }
+        }
+    }
+}
